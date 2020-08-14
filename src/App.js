a801367d4a2c2fe04363eedcf6488932cc9ac1db
Ervin <hamoydoerik@gmail.com>
2020-08-14 13:08:56 +0300

    Created application architecture. Added functionality for adding income / expenses

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Total from './components/total/Total';
+import History from './components/history/History';
+import Operation from './components/operation/Operation';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			transactions: [],
+			description: '',
+			amount: ''
+			
+		}
+	}
+
+	addTransaction = (add) => {
+		const transactions = [...this.state.transactions,
+			{
+				id: `cmr${(+new Date).toString(16)}`,
+				description: this.state.description,
+				amount: this.state.amount,
+				add
+			}
+		];
+
+		this.setState({
+			transactions,
+			description: '',
+			amount: '',
+		})
+	}
+
+	addAmount = (elem) => {
+		this.setState({amount: elem.target.value})
+	}
+
+	addDescription = (elem) => {
+		this.setState({description: elem.target.value})
+	}
+
+	render() {
+		return (
+			<>
+				<header>
+					<h1>Кошелек</h1>
+					<h2>Калькулятор расходов</h2>
+				</header>
+
+				<main>
+					<div className="container">
+						<Total/>
+						<History transactions={this.state.transactions}/>
+						<Operation 
+							addTransation={this.addTransaction}
+							addAmount={this.addAmount}
+							addDescription={this.addDescription}
+							description={this.state.description}
+							amount={this.state.amount}	
+						/>
+					</div>
+				</main>
+			</>
+		);
+	}
 }
-
-export default App;
